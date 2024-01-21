83c20d03ac39b06cb75ed3e0d569819d56a1d9ca
briankintu <brian2xa@gmail.com>
2024-01-21 20:53:24 +0000

    feat: Initialize App

---
+++ aicl/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Angalia Investment Consortium Limited",
+			"type": "module",
+			"label": _("Angalia Investment Consortium Limited")
+		}
+	]
