32b1729244863f9bde07e452fd819fcdedcdb829
briankintu <brian2xa@gmail.com>
2024-04-21 14:23:05 +0300

    seventh commit

---
+++ aicl/angalia_investment_consortium_limited/doctype/contact_website_message/test_contact_website_message.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Angalia Investment Consortium Limited and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestContactWebsiteMessage(FrappeTestCase):
+	pass
